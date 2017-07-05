67216041286042acf0ff1f5b782ef149688e9779
Kenny Lin <kenny.lin.91@gmail.com>
2017-07-05 14:05:45 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized = {}
+  data.each do |key, value|
+    value.each do |attr, pigeon|
+      pigeon.each do |name|
+        organized[name] = {} unless organized[name]
+        if organized[name][key]
+          organized[name][key].push(attr.to_s)
+        else
+          organized[name][key] = [attr.to_s]
+        end
+      end
+    end
+  end
+  organized
 end
